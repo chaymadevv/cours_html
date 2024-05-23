5681956255767a3eb53a479c608dd03b1102277b
chems <chayma.kilic@lepoles.org>
2024-05-23 16:53:03 +0200

    zoning php emet

--- zoning2/zoning.php
+++ zoning2/zoning.php
@@ -1,2 +1,14 @@
-<?php require"public/header.html.php" ?>
-<?php include__DIR__ ."/nav.html.php"; ?>
+<?php require "public/header.html.php" ?>
+
+<main>
+        <section class= "content">
+            <div class="A">A</div>
+            <div class="B">B</div>
+            <div class="C">C</div>
+            <div class="D">D</div>
+        </section>
+</main>
+
+<?php include "public/footer.html.php" ?>
+
+
---
+++ zoning2/zoning2.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="public/assets/css/style2.css">
+</head>
+<body>
+<!-- #container>header+nav+main>(section.sec$>div*4)*4^footer -->
+<div id="container">
+    <header>Header</header>
+    <nav>Nav</nav>
+    <main>
+        <section class="sec1">
+            <div>Petite Zone 1</div>
+            <div>Petite Zone 2</div>
+            <div>Petite Zone 3</div>
+            <div>Petite Zone 4</div>
+        </section>
+        <section class="sec2">
+            <div>Petite Zone 5</div>
+            <div>Petite Zone 6</div>
+            <div>Petite Zone 7</div>
+            <div>Petite Zone 8</div>
+        </section>
+        <section class="sec3">
+            <div>Petite Zone 9</div>
+            <div>Petite Zone 10</div>
+            <div>Petite Zone 11</div>
+            <div>Petite Zone 12</div>
+        </section>
+        <section class="sec4">
+            <div>Petite Zone 13</div>
+            <div>Petite Zone 14</div>
+            <div>Petite Zone 15 </div>
+            <div>Petite Zone 16</div>
+        </section>
+    </main>
+    <footer>Footer</footer>
+</div>
+</body>
+</html>
